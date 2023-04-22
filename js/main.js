cfd71d809b690ab8a6e9dfedf98e6e7771737d38
Nikhil <nikhilrb13@gmail.com>
2023-04-22 15:00:41 +0530

    Swiper js and css bundle added in projects

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+var swiper = new Swiper(".popular-content", {
+    slidePerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 755500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        280: {
+            slidePerView: 1,
+            spaceBetween: 10,
+        },
+        320: {
+            slidePerView: 2,
+            spaceBetween: 10,
+        },
+        510: {
+            slidePerView: 2,
+            spaceBetween: 10,
+        },
+        758: {
+            slidePerView: 3,
+            spaceBetween: 15,
+        },
+        900:{
+            slidePerView: 4,
+            spaceBetween: 20,
+        }
+    }
+});
